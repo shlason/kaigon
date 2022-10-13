fe97afecbc4d0b53803a7fef5440c9c08064e176
Jason <nocvi111@gmail.com>
2022-10-13 12:33:42 +0800

    feat: add redis configs and initial connection

--- models/models.go
+++ models/models.go
@@ -3,12 +3,14 @@ package models
 import (
 	"fmt"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/shlason/kaigon/configs"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var db *gorm.DB
+var rdb *redis.Client
 
 func init() {
 	dsn := fmt.Sprintf(
@@ -26,6 +28,13 @@ func init() {
 		panic(err)
 	}
 
+	rd := redis.NewClient(&redis.Options{
+		Addr:     configs.Database.Redis.Address,
+		Password: configs.Database.Redis.Password,
+		DB:       configs.Database.Redis.DB,
+	})
+
 	d.AutoMigrate(&Account{})
 	db = d
+	rdb = rd
 }
