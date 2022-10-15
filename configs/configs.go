86137fe4273114fb4b2c8d31e07590b306d3cb02
Jason <nocvi111@gmail.com>
2022-10-15 14:40:08 +0800

    chore: remove code

--- configs/configs.go
+++ configs/configs.go
@@ -50,11 +50,10 @@ func init() {
 
 	if err != nil {
 		fmt.Printf("Fail to get user home dir: %s\n", err)
+		os.Exit(1)
 	}
 
 	cfg, err := ini.Load(filepath.Join(homeDir, os.Getenv("CONFIG_FILE_PATH_BASE_ON_HOME")))
-
-	fmt.Println("Pr:", filepath.Join(homeDir, os.Getenv("CONFIG_FILE_PATH_BASE_ON_HOME")))
 
 	if err != nil {
 		fmt.Printf("Fail to read file: %v", err)
