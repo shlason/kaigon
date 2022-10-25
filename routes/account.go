f514447d3fab9ac03d2a441ce02db41d7e4eabb7
Jason <nocvi111@gmail.com>
2022-10-25 09:57:50 +0800

    feat: add account profile and setting routes

--- routes/account.go
+++ routes/account.go
@@ -8,10 +8,19 @@ import (
 func RegisteAccountRoutes(publicR *gin.RouterGroup, privateR *gin.RouterGroup) {
 	publicR.POST("/account/signup", account.SignUp)
 	publicR.POST("/account/signin", account.SignIn)
+
 	publicR.POST("/account/info/password/reset", account.CreateResetPasswordSession)
 	publicR.PATCH("/account/info/password/reset", account.ResetPassword)
 
+	privateR.POST("/account/:accountUUID/info/verification", account.CreateVerifySession)
 	publicR.GET("/account/:accountUUID/info/verification/email", account.VerifyWithEmail)
 
-	privateR.POST("/account/:accountUUID/info/verification", account.CreateVerifySession)
+	publicR.GET("/account/:accountUUID/profile")
+	privateR.PATCH("/account/:accountUUID/profile")
+
+	publicR.GET("/account/:accountUUID/setting")
+	privateR.PATCH("/account/:accountUUID/setting")
+
+	privateR.GET("/account/:accountUUID/setting/notification")
+	privateR.PUT("/account/:accountUUID/setting/notification")
 }
