de257cd6b2a64cfed5c181327c7c09744fac2e87
Jason <nocvi111@gmail.com>
2022-11-07 12:21:49 +0800

    feat: cookie domain config

--- models/constants/cookie.go
+++ models/constants/cookie.go
@@ -1,5 +1,11 @@
 package constants
 
+import (
+	"fmt"
+
+	"github.com/shlason/kaigon/configs"
+)
+
 type refreshTokenCookieInfo struct {
 	Name     string
 	MaxAge   int
@@ -14,7 +20,7 @@ var RefreshTokenCookieInfo = refreshTokenCookieInfo{
 	// Second Base -> 分 -> 小時 -> 天 -> 20 天
 	MaxAge:   60 * 60 * 24 * 20,
 	Path:     "/",
-	Domain:   "/",
+	Domain:   fmt.Sprintf(".%s", configs.Server.Host),
 	Secure:   false,
 	HttpOnly: true,
 }
