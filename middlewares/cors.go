d15f5c72c4fe134cc6b82a8cd903fc1667de8213
Jason <nocvi111@gmail.com>
2022-11-11 09:24:59 +0800

    feat: cookie setting

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -7,7 +7,8 @@ import (
 
 func CORS() gin.HandlerFunc {
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://google.com", "http://local.kaigon.sidesideeffect.io:8080"}
+	// TODO: 限制規則 regex
+	config.AllowOrigins = []string{"https://google.com", "http://local.kaigon.sidesideeffect.io:8080", "http://local.kaigon.sidesideeffect.io:3000"}
 	config.AllowMethods = []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS", "PUT"}
 	config.AllowHeaders = []string{"Authorization", "Content-Type", "Upgrade", "Origin",
 		"Connection", "Accept-Encoding", "Accept-Language", "Host", "Access-Control-Request-Method", "Access-Control-Request-Headers"}
