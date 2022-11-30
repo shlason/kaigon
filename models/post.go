c9ce16e6c8e027fefde85f2c84a56fbe48379de9
Jason <nocvi111@gmail.com>
2022-11-30 17:42:46 +0800

    feat: post model field naming

--- models/post.go
+++ models/post.go
@@ -4,11 +4,11 @@ const postsCollectionName string = "posts"
 
 type Post struct {
 	mongoDBModel
-	AccountID     uint
-	IsAnonymous   bool
-	ForumID       uint
+	AccountID     uint `bson:"account_id"`
+	IsAnonymous   bool `bson:"is_anonymous"`
+	ForumID       uint `bson:"forum_id"`
 	Title         string
 	Content       string
-	CommentCount  int
-	ReactionStats map[string]int
+	CommentCount  int            `bson:"comment_count"`
+	ReactionStats map[string]int `bson:"reaction_stats"`
 }
