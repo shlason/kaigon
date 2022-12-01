c4c48521c0a0daad29c93cc7465dcae04285a390
Jason <nocvi111@gmail.com>
2022-12-01 12:06:41 +0800

    feat: topic route

--- routes/topic.go
+++ routes/topic.go
@@ -4,5 +4,5 @@ import "github.com/gin-gonic/gin"
 
 func RegisteTopicRoutes(publicR *gin.RouterGroup, privateR *gin.RouterGroup) {
 	publicR.GET("/topics")
-	privateR.POST("/topics")
+	privateR.POST("/topics/followed")
 }
