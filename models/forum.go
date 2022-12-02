330f7e950c115485597a9753cfc1e4ea98b30a05
Jason <nocvi111@gmail.com>
2022-12-02 11:31:03 +0800

    fix: mongoDB anonymous struct scope level and add BSON inline tag

--- models/forum.go
+++ models/forum.go
@@ -1,13 +1,32 @@
 package models
 
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 const forumsCollectionName string = "forums"
 
 type Forum struct {
-	mongoDBModel
+	MongoDBModel  `bson:",inline"`
 	Name          string
 	Icon          string
 	Banner        string
 	Rule          string
 	Description   string
 	PopularTopics []string `bson:"popular_topics"`
+}
+
+func (f *Forum) InsertOne() (*mongo.InsertOneResult, error) {
+	current := time.Now().UTC()
+	f.CreatedAt = current
+	f.UpdatedAt = current
+	return mdb.Forums.InsertOne(context.TODO(), f)
+}
+
+func (f *Forum) FindOneByName() error {
+	return mdb.Forums.FindOne(context.TODO(), bson.D{{Key: "name", Value: f.Name}}).Decode(&f)
 }
