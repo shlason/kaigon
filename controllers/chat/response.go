0d6d1f005180bdad46d6250def9ddbbde9c699c9
Jason <nocvi111@gmail.com>
2022-12-04 17:23:45 +0800

    feat: create chat room controller

--- controllers/chat/response.go
+++ controllers/chat/response.go
@@ -1,8 +1,10 @@
 package chat
 
 const (
-	errCodeRequestFieldNotValid                string = "err-400-rfnv"
-	errMessageRequestFieldNotValid             string = "request some field is not valid"
-	errCodeRequestChatRoomInviteCodeExpired    string = "err-400-rcrice"
-	errMessageRequestChatRoomInviteCodeExpired string = "chat room invite code expired"
+	errCodeRequestFieldNotValid                    string = "err-400-rfnv"
+	errMessageRequestFieldNotValid                 string = "request some field is not valid"
+	errCodeRequestFieldInvitedUserEmailNotExist    string = "err-400-rfiuene"
+	errMessageRequestFieldInvitedUserEmailNotExist string = "invited user email not exist"
+	errCodeRequestChatRoomInviteCodeExpired        string = "err-400-rcrice"
+	errMessageRequestChatRoomInviteCodeExpired     string = "chat room invite code expired"
 )
