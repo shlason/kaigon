26e598b82da9b1be23d09ba0bbf74491aba8a134
Jason <nocvi111@gmail.com>
2022-12-04 15:55:31 +0800

    fix: mongoDB objectID field type

--- models/post_favorite.go
+++ models/post_favorite.go
@@ -1,9 +1,11 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 const postFavoriteCollectionName string = "post_favorite"
 
 type PostFavorite struct {
 	MongoDBModel `bson:",inline"`
-	PostID       uint `bson:"post_id"`
-	AccountID    uint `bson:"account_id"`
+	PostID       primitive.ObjectID `bson:"post_id"`
+	AccountID    uint               `bson:"account_id"`
 }
--- models/post_followed.go
+++ models/post_followed.go
@@ -1,9 +1,11 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 const postFollowedCollectionName string = "post_followed"
 
 type PostFollowed struct {
 	MongoDBModel `bson:",inline"`
-	PostID       uint `bson:"post_id"`
-	AccountID    uint `bson:"account_id"`
+	PostID       primitive.ObjectID `bson:"post_id"`
+	AccountID    uint               `bson:"account_id"`
 }
